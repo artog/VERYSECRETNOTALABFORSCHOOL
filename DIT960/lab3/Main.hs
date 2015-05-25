0eebbc51f08ad8eb285813edd8400a4b229b5766
artog <adam.ingmansson@gmail.com>
2015-05-25 08:45:59 +0200

    Implemented a few functions
    rightSub, inorder, size, maxheight, isSorted and checkRedParents + 3
    test trees

--- DIT960/lab3/Main.hs
+++ DIT960/lab3/Main.hs
@@ -1,11 +1,11 @@
 {-# OPTIONS -Wall #-}
 
 {-
-  to run:
-  $ ghc -e main Main.hs < swahili-small.txt
+    to run:
+    $ ghc -e main Main.hs < swahili-small.txt
 
-  to compile and run:
-  $ ghc -O3 --make Main.hs && ./Main < swahili-small.txt
+    to compile and run:
+    $ ghc -O3 --make Main.hs && ./Main < swahili-small.txt
 -}
 
 import RedBlackTree
@@ -14,15 +14,25 @@ import RedBlackTree
 
 main :: IO ()
 main = do
-  contents <- getContents
+    --contents <- getContents
 
-  -- split the data into words and build a red-black tree
-  -- use foldl
-  undefined
-
-  -- calculate and print statistics
-  -- use fromIntegral/ceiling/logBase
-  undefined
+    -- split the data into words and build a red-black tree
+    -- use foldl
+    --let tree = foldl (\t x -> insert x t) $ words contents
+    let tree    = tree1
+            n       = size tree
+            h       = fromIntegral $ maxheight tree
+            h'      = (logBase 2.0 ((fromIntegral n)+1))-  1.0 :: Double
+            ratio   = h / h'
+            valid   = checkTree tree
+            first20 = unwords $ map show $ take 20 $ inorder tree
+    -- calculate and print statistics
+    -- use fromIntegral/ceiling/logBase
+    putStr $ "Size: " ++ (show n) ++ "\n"
+    putStr $ "Height: " ++ (show h) ++ "\n"
+    putStr $ "Optimal height: " ++ (show h') ++ "\n"
+    putStr $ "Height / Optimal height: " ++ (show ratio) ++ "\n"
+    putStr $ "checkTree: " ++ (show valid) ++ "\n"
+    putStr $ "First 20 words: " ++ first20 ++ "\n"
 
 --------------------------------------------------------------------------------
-
--- DIT960/lab3/RedBlackTree.hs
+++ DIT960/lab3/RedBlackTree.hs
@@ -4,6 +4,7 @@
 
 module RedBlackTree (
   RBTree,        -- type of red-black search trees
+  tree1,
   emptyTree,     -- RBTree a
   isEmpty,       -- RBTree a -> Bool
   leftSub,       -- RBTree a -> RBTree a
@@ -25,7 +26,7 @@ import Control.Monad (guard)
 import Data.Maybe (isJust)
 
 -- red-black search trees
-data Color = R | B deriving (Show, Eq)
+data Color = R | B deriving (Show, Eq, Read)
 data RBTree a = Empty | Tree Color a (RBTree a) (RBTree a)
   deriving (Eq, Show, Read)
 
@@ -39,7 +40,8 @@ leftSub :: RBTree a -> RBTree a
 leftSub = undefined
 
 rightSub :: RBTree a -> RBTree a
-rightSub = undefined
+rightSub Empty = Empty
+rightSub (Tree _ _ _ t2) = t2
 
 rootVal :: RBTree a -> a
 rootVal = undefined
@@ -54,13 +56,16 @@ insert :: Ord a => a -> RBTree a -> RBTree a
 insert = undefined
 
 inorder :: RBTree a -> [a]
-inorder = undefined
+inorder Empty = []
+inorder (Tree _ a t1 t2) = (inorder t1) ++ [a] ++ (inorder t2) 
 
 size :: RBTree a -> Int
-size = undefined
+size Empty = 0
+size (Tree _ _ t1 t2) = 1 + size t1 + size t2
 
 maxheight :: RBTree a -> Int
-maxheight = undefined
+maxheight Empty = 0
+maxheight (Tree _ _ t1 t2) = 1 + max (maxheight t1) (maxheight t2)
 
 --------------------------------------------------------------------------------
 -- Optional function
@@ -80,11 +85,19 @@ checkTree root =
 
 -- True if the given list is ordered
 isSorted :: Ord a => [a] -> Bool
-isSorted = undefined
+isSorted [] = True
+isSorted (_:[]) = True
+isSorted (a:b:l) = (a <= b) && (isSorted (b:l))
 
 -- True if every red node only has black children
 checkRedParents :: RBTree a -> Bool
-checkRedParents = undefined
+checkRedParents Empty = True
+checkRedParents (Tree c _ t1 t2) | c == B    = and [checkRedParents t1, checkRedParents t2]
+checkRedParents (Tree _ _ t1 t2) | otherwise = and [valid, checkRedParents t1, checkRedParents t2]
+  where 
+    left  = isBlack t1
+    right = isBlack t2
+    valid = left && right
 
 -- If the number of black nodes from each leaf to the root
 -- are the same, then that number is returned,
@@ -93,6 +106,7 @@ checkBlackHeight :: RBTree a -> Maybe Int
 checkBlackHeight tree
   | isEmpty tree = do
       guard (isBlack tree)
+
       return 1
   | otherwise = do
       let left = leftSub tree
@@ -109,3 +123,10 @@ checkBlackHeight tree
         return lh
 
 --------------------------------------------------------------------------------
+
+tree1 :: RBTree Int
+tree1 = (Tree B 5 (Tree R 2 (Tree B 1 Empty Empty) Empty) (Tree R 6 Empty Empty))
+tree2 :: RBTree Int
+tree2 = (Tree R 5 (Tree B 2 (Tree R 3 Empty Empty) Empty) (Tree B 6 Empty Empty))
+tree3 :: RBTree Int
+tree3 = (Tree R 5 (Tree R 2 (Tree R 1 Empty Empty) Empty) (Tree B 6 Empty Empty))
