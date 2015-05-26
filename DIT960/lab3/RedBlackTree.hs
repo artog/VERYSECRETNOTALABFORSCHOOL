e462b426471cbdc686a9fa7c04add48378e8424a
Mikael <mkajsjo2@gmail.com>
2015-05-26 11:38:32 +0200

    comments and time complexity

--- DIT960/lab3/RedBlackTree.hs
+++ DIT960/lab3/RedBlackTree.hs
@@ -30,29 +30,37 @@ data Color = R | B deriving (Show, Eq, Read)
 data RBTree a = Empty | Tree Color a (RBTree a) (RBTree a)
   deriving (Eq, Show, Read)
 
+
+-- O(1)
 emptyTree :: RBTree a
 emptyTree = Empty
 
+-- O(1)
 isEmpty :: RBTree a -> Bool
 isEmpty Empty         = True
 isEmpty _             = False
 
+-- O(1)
 leftSub :: RBTree a -> RBTree a
 leftSub Empty             = Empty
 leftSub (Tree _ _ left _) = left
 
+-- O(1)
 rightSub :: RBTree a -> RBTree a
 rightSub Empty = Empty
 rightSub (Tree _ _ _ right) = right
 
+-- O(1)
 rootVal :: RBTree a -> a
 rootVal Empty              = undefined
 rootVal (Tree _ value _ _) = value
 
+-- O(1)
 isBlack :: RBTree a -> Bool
 isBlack (Tree R _ _ _) = False
 isBlack _              = True
 
+-- O(log n)
 get :: Ord a => a -> RBTree a -> Maybe a
 get _ Empty = Nothing
 get value tree 
@@ -60,19 +68,28 @@ get value tree
   | value > rootVal tree  = get value $ rightSub tree
   | otherwise             = Just $ rootVal  tree
 
+-- O(log n)
+-- Inserts a value in the tree as a red node. If the new node's parent
+-- is a red node we have broken the invariant. Then we rotate and recolor
+-- the tree, using pattern matching, to satisfy the invariant.
+-- There are four different cases of rebalancing that needs to be done
+-- depending on if the new node and its parent is a left/right node. 
+-- If the root of the tree is red we recolour it.
 insert :: Ord a => a -> RBTree a -> RBTree a
 insert value tree  = makeRootBlack (insert' value tree)
   where makeRootBlack (Tree _ a l r) = (Tree B a l r) 
 
-
+-- O(n)
 inorder :: RBTree a -> [a]
 inorder Empty = []
 inorder (Tree _ a t1 t2) = (inorder t1) ++ [a] ++ (inorder t2) 
 
+-- O(n)
 size :: RBTree a -> Int
 size Empty = 0
 size (Tree _ _ t1 t2) = 1 + size t1 + size t2
 
+-- O(n)
 maxheight :: RBTree a -> Int
 maxheight Empty = 0
 maxheight (Tree _ _ t1 t2) = 1 + max (maxheight t1) (maxheight t2)
@@ -81,6 +98,8 @@ maxheight (Tree _ _ t1 t2) = 1 + max (maxheight t1) (maxheight t2)
 --------------------------------------------------------------------------------
 -- Helpers
 
+-- O(log n)
+-- For description see function insert's comment
 insert' :: Ord a => a -> RBTree a -> RBTree a
 insert' value Empty = (Tree R value Empty Empty)
 insert' value t@(Tree c a l r)
@@ -88,11 +107,19 @@ insert' value t@(Tree c a l r)
   | value > a = balanceRight (Tree c a l (insert' value r))
   | otherwise = t 
 
+
+-- O(1)
+-- Rebalances a left-left tree or a left-right tree
+-- (Rotates and recolours using pattern matching)
 balanceLeft :: RBTree a -> RBTree a
 balanceLeft (Tree B z (Tree R y (Tree R x a b) c) d) = (Tree R y (Tree B x a b) (Tree B z c d))
 balanceLeft (Tree B z (Tree R x a (Tree R y b c)) d) = (Tree R y (Tree B x a b) (Tree B z c d))
 balanceLeft t = t
 
+
+-- O(1)
+-- Rebalances a right-right tree or a right-left tree 
+-- (Rotates and recolours using pattern matching)
 balanceRight :: RBTree a -> RBTree a
 balanceRight (Tree B x a (Tree R y b (Tree R z c d))) = (Tree R y (Tree B x a b) (Tree B z c d))
 balanceRight (Tree B x a (Tree R z (Tree R y b c) d)) = (Tree R y (Tree B x a b) (Tree B z c d))
