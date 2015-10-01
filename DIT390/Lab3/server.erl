a16cc323215734c656be0e14cf25363c3e5335b6
Mikael <mkajsjo2@gmail.com>
2015-10-01 12:53:31 +0200

    connect and disconnect with error...

--- DIT390/Lab3/server.erl
+++ DIT390/Lab3/server.erl
@@ -4,9 +4,22 @@
 
 % Produce initial state
 initial_state(ServerName) ->
-    #server_st{}.
+    #server_st{name = ServerName, clients = []}.
 
 %% ---------------------------------------------------------------------------
 
 loop(St, Message) ->
-    {not_implemented, St}.
+    Clients  = St#server_st.clients,
+    Channels = St#server_st.channels,
+    case Message of
+        {connect, Name} ->
+            case lists:member(Name, Clients) of
+                false -> {ok, St#server_st{clients=[Name|Clients]}};
+                true  -> {user_already_connected, St}
+            end;
+        {disconnect, Name} ->
+            case lists:member(Name, Clients) of
+                false -> {user_not_connected, St};
+                true  -> {ok, St#server_st{clients=lists:delete(Name, Clients)}}
+            end
+    end.
